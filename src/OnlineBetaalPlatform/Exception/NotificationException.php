b5e8754ca4e7bfcb92e69892c4262d4bbea3253e
Martijn <martijn@greenhighway.nl>
2020-05-13 15:41:24 +0200

    Added NotificationException to catch exceptions

---
+++ src/OnlineBetaalPlatform/Exception/NotificationException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OnlineBetaalPlatform\Exception;
+
+/**
+ * Is thrown when an exception occures when receiving notifications from the payment system
+ */
+class NotificationException extends OBPException
+{
+
+}
--- src/OnlineBetaalPlatform/Manager/NotificationsManager.php
+++ src/OnlineBetaalPlatform/Manager/NotificationsManager.php
@@ -2,7 +2,9 @@
 
 namespace OnlineBetaalPlatform\Manager;
 
+use Exception;
 use JsonMapper;
+use OnlineBetaalPlatform\Exception\NotificationException;
 use OnlineBetaalPlatform\Model\Notification;
 
 /**
@@ -23,7 +25,11 @@ class NotificationsManager
 
     public function processNotification($jsonMessage): Notification
     {
-        $data = json_decode($jsonMessage);
-        return $this->mapper->map($data, new Notification());
+        try {
+            $data = json_decode($jsonMessage);
+            return $this->mapper->map($data, new Notification());
+        } catch (Exception $exception) {
+            throw new NotificationException('Unable to process notification: ' . $exception->getMessage(), $exception->getCode(), $exception);
+        }
     }
 }
